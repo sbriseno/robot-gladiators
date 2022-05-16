a75fc105fee9506579fe2e166a4bb5ae0cd54ba8
sergio <sgbriseno95@gmail.com>
2022-05-16 11:04:41 -0500

    removed nested folder

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,26 @@
 var playerName = window.prompt("What is your robot's name?");
-// What is this?
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-console.log(10+10);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("Our robot's name is" + playerName)
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
 
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-  }
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  enemyHealth = enemyHealth - playerAttack;
+  // Log a resulting message to the console so we know that it worked.
+  console.log(
+    playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining"
+  )
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 
-// fight();
+  // Log a resulting message to the console so we know that it worked.
+};
+
+fight();
